ae478a7b90968b2a1c17da0791d986c042055e41
fits <261649+fits@users.noreply.github.com>
2026-08-05 20:00:58 +0900

    mdo macro example

---
+++ example/src/example5.rs
@@ -0,0 +1,41 @@
+use macuru::{MonadLike, mdo};
+
+fn main() -> Result<(), ()> {
+    let d1 = mdo!(
+        x <- vec![1, 2]
+        yield x + 1
+    );
+
+    println!("d1 = {:?}", d1);
+
+    let v1 = vec![1, 3, 5];
+
+    let d2 = mdo!(
+            x <- vec!["a", "b"]
+            y <- v1.clone()
+            yield (x, y * 2)
+    );
+
+    println!("d2 = {:?}", d2);
+
+    let f = |v: i32| if v > 3 { Some(v) } else { None };
+
+    let d3 = mdo!(
+        x <- Some("a")
+        y <- Some("b")
+        z <- f(5)
+        yield format!("{}-{}-{}", x, y, z)
+    );
+
+    println!("d3 = {:?}", d3);
+
+    let d4 = mdo!(
+        a <- f(10)
+        b <- f(3)
+        yield (a, b)
+    );
+
+    println!("d4 = {:?}", d4);
+
+    Ok(())
+}
